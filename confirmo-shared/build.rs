f375cb57c99c678c0d8069396fe4e39dc4ff5566
Wesley-Arizio <wesleyarizio26@gmail.com>
2026-03-07 16:11:06 -0300

    feat: use feature flags for grpc and kafka usage

--- confirmo-shared/build.rs
+++ confirmo-shared/build.rs
@@ -1,5 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure().compile_protos(&["proto/auth/auth.proto"], &["proto/auth"])?;
+    if std::env::var_os("CARGO_FEATURE_GRPC").is_some() {
+        #[cfg(feature = "grpc")]
+        tonic_prost_build::configure()
+            .compile_protos(&["proto/auth/auth.proto"], &["proto/auth"])?;
+    }
 
     Ok(())
 }
--- confirmo-shared/src/lib.rs
+++ confirmo-shared/src/lib.rs
@@ -1,6 +1,11 @@
 pub mod auth_event;
+
+#[cfg(feature = "grpc")]
 pub mod auth {
     tonic::include_proto!("auth");
 }
+
 pub mod constants;
+
+#[cfg(feature = "kafka")]
 pub mod kafka;
