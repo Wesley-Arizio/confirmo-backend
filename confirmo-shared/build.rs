f89fd8e89cc8469bc83b17afeff1989bff4811ff
Wesley-Arizio <wesleyarizio26@gmail.com>
2026-03-08 14:53:02 -0300

    fix: include directory to proto on build

--- confirmo-shared/build.rs
+++ confirmo-shared/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::env::var_os("CARGO_FEATURE_GRPC").is_some() {
         #[cfg(feature = "grpc")]
         tonic_prost_build::configure()
-            .compile_protos(&["proto/auth/auth.proto"], &["proto/auth"])?;
+            .compile_protos(&["proto/auth/auth.proto"], &["proto/auth", "/usr/include"])?;
     }
 
     Ok(())
